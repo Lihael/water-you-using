ec41ecaa7e0fc92aeb0b9877c45e633d38706eaa
seichen <seichen@purdue.edu>
2020-01-25 16:09:48 -0500

    Update Tip.js

--- src/Tip.js
+++ src/Tip.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 class Tips extends React.Component{
-    state = {tips: ["Don't keep the faucet running while brushing youur teeth!", 
+    state = {tips: ["Don't keep the faucet running while brushing your teeth!", 
     "Get a toilet cistern from your water provider",
     "Take shorter showers! Aerated shower heads and shower regulators can decrease shower water usage as well.",
     "Use full loads in the dishwasher and the laundry machine",
