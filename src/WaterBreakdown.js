06ac7387c1a45367d217975feae1eefe5794e43f
Lihael <Lihael@users.noreply.github.com>
2020-01-25 11:04:39 -0500

    water update

--- src/WaterBreakdown.js
+++ src/WaterBreakdown.js
@@ -1,16 +1,14 @@
 import React from 'react'
-import semReact from 'semantic-ui-react';
+import { Progress } from 'semantic-ui-react'
 import ReactInterval from 'react-interval';
 
 
 class WaterBreakdown extends React.Component{
     state = {time: 0}
     render(){
-        const time = this.state.time
+        
         return <div>
-        <ReactInterval timeout={1000} enabled={true}
-          callback={() => this.setState({count: this.state.count + 1})} />
-        <semReact.Progress progress='value' value={35} total={50} />
+      <Progress progress='value' value={35} total={50} />
         </div>
     }
 }
