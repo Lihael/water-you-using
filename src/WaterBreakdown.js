9ee9fc038392fe7965bcf75d7abf8c14d5374fb2
Lihael <Lihael@users.noreply.github.com>
2020-01-25 11:18:15 -0500

    Revert "water update"
    This reverts commit 06ac7387c1a45367d217975feae1eefe5794e43f.

--- src/WaterBreakdown.js
+++ src/WaterBreakdown.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import { Progress } from 'semantic-ui-react'
+import semReact from 'semantic-ui-react';
 import ReactInterval from 'react-interval';
 
 
 class WaterBreakdown extends React.Component{
     state = {time: 0}
     render(){
-        
+        const time = this.state.time
         return <div>
-      <Progress progress='value' value={35} total={50} />
+        <ReactInterval timeout={1000} enabled={true}
+          callback={() => this.setState({count: this.state.count + 1})} />
+        <semReact.Progress progress='value' value={35} total={50} />
         </div>
     }
 }
